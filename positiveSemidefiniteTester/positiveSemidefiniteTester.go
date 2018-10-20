7b0b5da954daa27d2dc87192fbd1a1d85bfbe895
acra5y <oli4@acrasy.net>
2018-10-20 21:28:52 +0200

    Stop after first imaginary or negative value

--- positiveSemidefiniteTester/positiveSemidefiniteTester.go
+++ positiveSemidefiniteTester/positiveSemidefiniteTester.go
@@ -2,7 +2,6 @@ package positiveSemidefiniteTester
 
 import (
 	"gonum.org/v1/gonum/mat"
-	"math"
 	"math/cmplx"
 )
 
@@ -15,14 +14,18 @@ func isSymmetric(a mat.Matrix) bool {
 }
 
 func (candidate PositiveSemidefiniteCandidate) IsPositiveSemidefinite() (isPositiveSemidefinite bool) {
+	if !isSymmetric(candidate.Value) {
+		return false
+	}
 	eigen := mat.Eigen{}
 	eigen.Factorize(candidate.Value, false, false)
 	isPositiveSemidefinite = true
 
 	for _, val := range eigen.Values(nil) {
 		_, theta := cmplx.Polar(val)
-		if theta != math.Pi && theta != 0 {
+		if theta != 0 {
 			isPositiveSemidefinite = false
+			return
 		}
 	}
 	return
