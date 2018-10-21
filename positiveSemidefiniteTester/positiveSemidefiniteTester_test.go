51729152f344eb2288fb09f98a202cd4d67d9568
Oli <oli.schmelzle@gmail.com>
2018-10-21 16:21:57 +0200

    Handle not ok from Eigen.Factorize

--- positiveSemidefiniteTester/positiveSemidefiniteTester_test.go
+++ positiveSemidefiniteTester/positiveSemidefiniteTester_test.go
@@ -1,53 +1,65 @@
 package positiveSemidefiniteTester
 
 import (
+	"fmt"
 	"gonum.org/v1/gonum/mat"
+	"reflect"
 	"testing"
 )
 
 type EigenMock struct {
-	MockData []complex128
+	factorizeOk bool
+	mockData []complex128
 }
 
 func (eigen EigenMock) Factorize(a mat.Matrix, left, right bool) bool {
-	return true
+	return eigen.factorizeOk
 }
 
 func (eigen EigenMock) Values(dst []complex128) []complex128 {
-	return eigen.MockData
+	return eigen.mockData
 }
 
-func createEigenMock(values []complex128) EigenMock {
-	eigen := EigenMock{ MockData: values }
+func createEigenMock(factorizeOk bool, values []complex128) EigenMock {
+	eigen := EigenMock{ mockData: values, factorizeOk: factorizeOk }
 	return eigen
 }
 
 var dummyMatrix = mat.NewDense(2, 2, []float64{0,0,0,0})
 
-func TestPsdForSymmetricMatrix(t *testing.T) {
+func TestPsdForMatrix(t *testing.T) {
     tables := []struct {
+    	value mat.Matrix
 		values []complex128
 		isPsd bool
+		factorizeOk bool
 	}{
-		{values: []complex128{0,5}, isPsd: true},
-		{values: []complex128{0,-5}, isPsd: false},
-		{values: []complex128{0,complex(5, 7)}, isPsd: false},
-		//{matrix: mat.NewDense(2, 2, []float64{1,2,3,4}), isPsd: false},
+		{values: []complex128{0,5}, isPsd: true, factorizeOk: true, value: dummyMatrix},
+		{values: []complex128{0,-5}, isPsd: false, factorizeOk: true, value: dummyMatrix},
+		{values: []complex128{0,complex(5, 7)}, isPsd: false, factorizeOk: true, value: dummyMatrix},
+		{values: []complex128{}, isPsd: false, factorizeOk: true, value: mat.NewDense(2, 2, []float64{0,1,0,0})},
 	}
 
 	for _, table := range tables {
-	candidate := PositiveSemidefiniteCandidate{ Value: dummyMatrix }
-    isPsd := candidate.IsPositiveSemidefinite(createEigenMock(table.values))
+	candidate := PositiveSemidefiniteCandidate{ Value: table.value }
+    isPsd, err := candidate.IsPositiveSemidefinite(createEigenMock(table.factorizeOk, table.values))
+        if err != nil {
+			t.Errorf("IsPositiveSemidefinite returned unexpected error: %v", err)
+		}
 		if isPsd != table.isPsd {
 			t.Errorf("IsPositiveSemidefinite was incorrect, got: %t, want: %t.", isPsd, table.isPsd)
 		}
 	}
 }
 
-func TestPsdForNotSymmetricMatrix(t *testing.T) {
-	candidate := PositiveSemidefiniteCandidate{ Value: mat.NewDense(2, 2, []float64{0,1,0,0}) }
-	isPsd := candidate.IsPositiveSemidefinite(createEigenMock([]complex128{0,5}))
-	if isPsd != false {
+func TestPsdFactorizeNotOk(t *testing.T) {
+	candidate := PositiveSemidefiniteCandidate{ Value: dummyMatrix }
+	isPsd, err := candidate.IsPositiveSemidefinite(createEigenMock(false, []complex128{}))
+	expectedError := fmt.Errorf("eigen: Factorize unsuccessful %v", mat.Formatted(candidate.Value, mat.Prefix("    "), mat.Squeeze()))
+	if !reflect.DeepEqual(err, expectedError) {
+		t.Errorf("Wrong error returned, got: %t, want: %t.", err, expectedError)
+	}
+	if isPsd {
 		t.Errorf("IsPositiveSemidefinite was incorrect, got: %t, want: %t.", isPsd, false)
 	}
 }
