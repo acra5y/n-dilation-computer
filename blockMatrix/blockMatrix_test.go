62e14febd15d0e85bdb82d19d7e2d69fd8a75d78
Oli <oli.schmelzle@gmail.com>
2018-12-25 16:35:01 +0100

    Add test cases

--- blockMatrix/blockMatrix_test.go
+++ blockMatrix/blockMatrix_test.go
@@ -33,7 +33,36 @@ func TestNewBlockMatrixFromSquares(t *testing.T) {
         expected *mat.Dense
         ok bool
     }{
-        {rows: createRows(), expected: mat.NewDense(4, 4, []float64{0,0,1,1,0,0,1,1,2,2,3,3,2,2,3,3}), desc: "returns correct matrix", ok: true},
+        {
+            rows: createRows(2, 2),
+            expected: mat.NewDense(4, 4, []float64{0,0,1,1,0,0,1,1,2,2,3,3,2,2,3,3,}),
+            desc: "returns correct matrix for 4 2x2 blocks",
+            ok: true,
+        },
+        {
+            rows: createRows(3, 2),
+            expected: mat.NewDense(6, 6, []float64{0,0,1,1,2,2,0,0,1,1,2,2,3,3,4,4,5,5,3,3,4,4,5,5,6,6,7,7,8,8,6,6,7,7,8,8,}),
+            desc: "returns correct matrix for 9 2x2 blocks",
+            ok: true,
+        },
+        {
+            rows: createRows(2, 3),
+            expected: mat.NewDense(6, 6, []float64{0,0,0,1,1,1,0,0,0,1,1,1,0,0,0,1,1,1,2,2,2,3,3,3,2,2,2,3,3,3,2,2,2,3,3,3,}),
+            desc: "returns correct matrix for 4 3x3 blocks",
+            ok: true,
+        },
+        {
+            rows: createRows(5, 1),
+            expected: mat.NewDense(5, 5, []float64{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,}),
+            desc: "returns correct matrix for 25 1x1 blocks",
+            ok: true,
+        },
+        {
+            rows: createRows(1, 5),
+            expected: mat.NewDense(5, 5, nil),
+            desc: "returns correct matrix for 1 5x5 blocks",
+            ok: true,
+        },
     }
 
     for _, table := range tables {
