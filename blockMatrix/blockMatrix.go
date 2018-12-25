ab1b634daabc1f421668acee00adc93e2de50513
acra5y <oli4@acrasy.net>
2018-12-25 16:44:40 +0100

    Refactor to calculate static values only once

--- blockMatrix/blockMatrix.go
+++ blockMatrix/blockMatrix.go
@@ -6,19 +6,23 @@ import (
 )
 
 func NewBlockMatrixFromSquares(rows [][]*mat.Dense) (*mat.Dense, bool) {
-    var d0, d int
+    var d0, d, entriesPerBlock, entriesPerBlockRow, blockDim int
     d0, _ = rows[0][0].Dims()
-    d = d0 * len(rows)
+    entriesPerBlock = int(math.Pow(float64(d0), 2))
+    blockDim = len(rows)
+    entriesPerBlockRow = entriesPerBlock * blockDim
+
+    d = d0 * blockDim
 
     var data []float64
     data = make([]float64, int(math.Pow(float64(d), 2)))
 
     for i, _ := range rows {
+        offsetHandledBlockRows := i * entriesPerBlockRow
         for j := 0; j < d0; j++ {
+            offsetCurrentBlocks := j * d
             for k, matrix := range rows[i] {
                 raw := matrix.RawRowView(j)
-                offsetHandledBlockRows := int(math.Pow(float64(d0), 2)) * len(rows)
-                offsetCurrentBlocks := j * len(rows[i]) * d0
                 offsetHandledBlocks := k * d0
                 index := i * offsetHandledBlockRows + offsetCurrentBlocks + offsetHandledBlocks
 
