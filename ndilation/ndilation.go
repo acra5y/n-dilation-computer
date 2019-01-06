bdf4bb8af7efad1c9818ebb4571166ca56517531
Oli <oli.schmelzle@gmail.com>
2019-01-06 22:00:15 +0100

    Adapt dependency definition

--- ndilation/ndilation.go
+++ ndilation/ndilation.go
@@ -10,23 +10,17 @@ type Dilation struct {
 	nDilation *mat.Dense
 }
 
-type validation interface {
-	IsPositiveDefinite(positiveDefinite.EigenComputer) (bool, error)
-}
+type isPositiveDefinite func(positiveDefinite.EigenComputer) (bool, error)
 
-type squareRoot interface {
-	Calculate(*mat.Dense) (*mat.Dense, error)
-}
+type squareRoot func(*mat.Dense) (*mat.Dense, error)
 
-type blockMatrix interface {
-	NewBlockMatrixFromSquares([][]*mat.Dense) (*mat.Dense, error)
-}
+type newBlockMatrixFromSquares func([][]*mat.Dense) (*mat.Dense, error)
 
 func (Dilation *Dilation) Value() *mat.Dense {
 	return Dilation.nDilation
 }
 
-func (Dilation *Dilation) UnitaryNDilation(validation validation, squareRoot squareRoot, blockMatrix blockMatrix, t *mat.Dense) (error) {
+func (Dilation *Dilation) UnitaryNDilation(isPD isPositiveDefinite, sqrt squareRoot, newBlockMatrix newBlockMatrixFromSquares, t *mat.Dense) (error) {
 	dummy := mat.NewDense(2, 2, nil)
 	Dilation.nDilation = dummy
 	return nil
