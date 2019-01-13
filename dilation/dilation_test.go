4fa28c05aa67f0c25c68e7fa3df346b5e56ccda3
acra5y <oli4@acrasy.net>
2019-01-13 21:36:45 +0100

    Convert tabs to whitespace

--- dilation/dilation_test.go
+++ dilation/dilation_test.go
@@ -1,39 +1,39 @@
 package dilation
 
 import (
-	"github.com/acra5y/n-dilation-computer/positiveDefinite"
-	"gonum.org/v1/gonum/mat"
-	"testing"
+    "github.com/acra5y/n-dilation-computer/positiveDefinite"
+    "gonum.org/v1/gonum/mat"
+    "testing"
 )
 
 func isPositiveDefiniteMock(a positiveDefinite.EigenComputer, candidate *mat.Dense) (bool, error) {
-	return true, nil
+    return true, nil
 }
 
 func squareRootMock(a *mat.Dense) (*mat.Dense, error) {
-	return mat.NewDense(2, 2, nil), nil
+    return mat.NewDense(2, 2, nil), nil
 }
 
 func newBlockMatrixFromSquaresMock(a [][]*mat.Dense) (*mat.Dense, error) {
-	return mat.NewDense(2, 2, nil), nil
+    return mat.NewDense(2, 2, nil), nil
 }
 
 func TestUnitaryNDilation(t *testing.T) {
     tables := []struct {
-    	value *mat.Dense
-	}{
-		{value: mat.NewDense(2, 2, nil)},
-	}
+        value *mat.Dense
+    }{
+        {value: mat.NewDense(2, 2, nil)},
+    }
 
-	for _, table := range tables {
-		unitary, err := UnitaryNDilation(isPositiveDefiniteMock, squareRootMock, newBlockMatrixFromSquaresMock, table.value)
+    for _, table := range tables {
+        unitary, err := UnitaryNDilation(isPositiveDefiniteMock, squareRootMock, newBlockMatrixFromSquaresMock, table.value)
 
-		if err != nil {
-			t.Errorf("Unexpected err, want: %v, got: %v", nil, err)
-		}
+        if err != nil {
+            t.Errorf("Unexpected err, want: %v, got: %v", nil, err)
+        }
 
-		if !mat.Equal(unitary, table.value) {
-			t.Errorf("Wrong matrix returned, want: %v, got: %v", mat.NewDense(4, 4, nil), unitary)
-		}
-	}
+        if !mat.Equal(unitary, table.value) {
+            t.Errorf("Wrong matrix returned, want: %v, got: %v", mat.NewDense(4, 4, nil), unitary)
+        }
+    }
 }
