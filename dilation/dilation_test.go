408547c1e524211320d1785c288d114956c7814a
Oli <oli.schmelzle@gmail.com>
2019-01-16 22:04:26 +0100

    Test argument in sqrt

--- dilation/dilation_test.go
+++ dilation/dilation_test.go
@@ -10,8 +10,15 @@ func isPositiveDefiniteMock(a positiveDefinite.EigenComputer, candidate *mat.Den
     return true, nil
 }
 
-func squareRootMock(a *mat.Dense) (*mat.Dense, error) {
-    return mat.NewDense(2, 2, nil), nil
+func testSquareRoot(t *testing.T, expected []*mat.Dense) squareRoot {
+    calls := 0
+    return func(a *mat.Dense) (*mat.Dense, error) {
+        if !mat.Equal(expected[calls], a) {
+            t.Errorf("Unexpected argument in call %d to squareRoot. Got: %v, want: %v", calls + 1, a, expected)
+        }
+        calls++
+        return mat.NewDense(2, 2, nil), nil
+    }
 }
 
 func newBlockMatrixFromSquaresMock(a [][]*mat.Dense) (*mat.Dense, error) {
@@ -21,12 +28,13 @@ func newBlockMatrixFromSquaresMock(a [][]*mat.Dense) (*mat.Dense, error) {
 func TestUnitaryNDilation(t *testing.T) {
     tables := []struct {
         value *mat.Dense
+        expectedInSqrt []*mat.Dense
     }{
-        {value: mat.NewDense(2, 2, nil)},
+        {value: mat.NewDense(2, 2, nil), expectedInSqrt: []*mat.Dense{mat.NewDense(2, 2, []float64{1,0,0,1,}),mat.NewDense(2, 2, []float64{1,0,0,1,}),},},
     }
 
     for _, table := range tables {
-        unitary, err := UnitaryNDilation(isPositiveDefiniteMock, squareRootMock, newBlockMatrixFromSquaresMock, table.value)
+        unitary, err := UnitaryNDilation(isPositiveDefiniteMock, testSquareRoot(t, table.expectedInSqrt), newBlockMatrixFromSquaresMock, table.value)
 
         if err != nil {
             t.Errorf("Unexpected err, want: %v, got: %v", nil, err)
